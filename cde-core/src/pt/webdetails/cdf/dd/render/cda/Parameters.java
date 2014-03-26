7d676e379b9140cc1814be2ed9010d6b99dedb49
rdaniels6813 <rdaniels6813@gmail.com>
2014-03-26 09:37:24 -0400

    Fixed issue with saving Dashboards
    Minor issue with a dashboard returning JSONNull for default value in
    Parameters.java.  Used toString() method instead of (String) casting so
    that JSONNull will properly correlate to null strign value.

--- cde-core/src/pt/webdetails/cdf/dd/render/cda/Parameters.java
+++ cde-core/src/pt/webdetails/cdf/dd/render/cda/Parameters.java
@@ -5,57 +5,58 @@
 package pt.webdetails.cdf.dd.render.cda;
 
 import java.util.Iterator;
+
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+
 import org.apache.commons.jxpath.JXPathContext;
+import org.apache.commons.lang.StringUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.apache.commons.lang.StringUtils;
 
 /**
- *
+ * 
  * @author pdpi
  */
 public class Parameters implements CdaElementRenderer {
 
-  private JSONObject definition;
-  private final String NAME_ATTR = "name",
-                       DEFAULT_ATTR = "default",
-                       TYPE_ATTR = "type",
-                       ACCESS_ATTR = "access",
-                       ELEMENT_NAME = "Parameter";
+	private JSONObject definition;
+	private final String NAME_ATTR = "name", DEFAULT_ATTR = "default",
+			TYPE_ATTR = "type", ACCESS_ATTR = "access",
+			ELEMENT_NAME = "Parameter";
 
-  public void renderInto(Element dataAccess) {
-    JXPathContext.newContext(definition);
-    Document doc = dataAccess.getOwnerDocument();
-    Element parameters = doc.createElement("Parameters");
-    dataAccess.appendChild(parameters);
-    JSONArray params = JSONArray.fromObject(definition.getString("value"));
-    @SuppressWarnings("unchecked")
-    Iterator<JSONArray> paramIterator = params.iterator();
-    while (paramIterator.hasNext()) {
-      JSONArray param = (JSONArray) paramIterator.next();
-      Element parameter = doc.createElement(ELEMENT_NAME);
-      parameter.setAttribute(NAME_ATTR, (String) param.get(0));
-      parameter.setAttribute(DEFAULT_ATTR, (String) param.get(1));
-      if(param.size() > 2){
-        parameter.setAttribute(TYPE_ATTR,  (String) param.get(2));
-        if(param.size() > 3){
-          String access = (String) param.get(3);
-          if(!StringUtils.isEmpty(access)){
-            parameter.setAttribute(ACCESS_ATTR, access);
-          }
-        }
-      }
-      else{
-        parameter.setAttribute(TYPE_ATTR, "String");
-      }
-      
-      parameters.appendChild(parameter);
-    }
-  }
+	@Override
+	public void renderInto(Element dataAccess) {
+		JXPathContext.newContext(definition);
+		Document doc = dataAccess.getOwnerDocument();
+		Element parameters = doc.createElement("Parameters");
+		dataAccess.appendChild(parameters);
+		JSONArray params = JSONArray.fromObject(definition.getString("value"));
+		@SuppressWarnings("unchecked")
+		Iterator<JSONArray> paramIterator = params.iterator();
+		while (paramIterator.hasNext()) {
+			JSONArray param = paramIterator.next();
+			Element parameter = doc.createElement(ELEMENT_NAME);
+			parameter.setAttribute(NAME_ATTR, (String) param.get(0));
+			parameter.setAttribute(DEFAULT_ATTR, param.get(1).toString());
+			if (param.size() > 2) {
+				parameter.setAttribute(TYPE_ATTR, (String) param.get(2));
+				if (param.size() > 3) {
+					String access = (String) param.get(3);
+					if (!StringUtils.isEmpty(access)) {
+						parameter.setAttribute(ACCESS_ATTR, access);
+					}
+				}
+			} else {
+				parameter.setAttribute(TYPE_ATTR, "String");
+			}
 
-  public void setDefinition(JSONObject definition) {
-    this.definition = definition;
-  }
+			parameters.appendChild(parameter);
+		}
+	}
+
+	@Override
+	public void setDefinition(JSONObject definition) {
+		this.definition = definition;
+	}
 }
